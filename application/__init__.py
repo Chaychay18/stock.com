cc83e851fb64da092153ebd1cdd37ade1c945b7d
chaithanya <chaithuchaithanya587@gmail.com>
2024-10-28 18:45:57 +0530

    sqlite update

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
 
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SECRET_KEY'] = '767656chay'
 #account_sid = 'ACd0c312ca8abe224bdee296a629f0b930'
 #auth_token = 'c9215cc3a49810beb88c955f326e7179'
